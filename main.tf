f1e33cf582cb0eb2dccdd43909a265d311283c3e
deresolution20 <85902399+deresolution20@users.noreply.github.com>
2023-01-04 14:44:36 -0700

    lambda policy added

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/*
+Name: IaC Buildout for Cloud Resume Challenge
+Description: AWS Infrastructure Buildout
+Created by: Brice Neal
+*/
+
 terraform {
   cloud {
     organization = "bn-tf"
@@ -35,4 +41,67 @@ resource "aws_dynamodb_table_item" "tf_visitorcounttable_items" {
             "record_count":{"N": "551"}
         }
     EOF
+}
+
+## LAMBDA
+# create role for lambda usage
+resource "aws_iam_role" "tf_LambdaDynamoDBRole" {
+    name = "tf_LambdaDynamoDBRole"
+    assume_role_policy = <<EOF
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "lambda.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }   
+            ]
+        }
+EOF
+}
+
+# create policy for above role (to set permissions for the role)
+resource "aws_iam_policy" "tf_LambdaDynamoDBPolicy" {
+    name = "tf_LambdaDynamoDBPolicy"
+    policy = jsonencode(
+        {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:BatchGetItem",
+                "dynamodb:GetItem",
+                "dynamodb:Query",
+                "dynamodb:Scan",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:PutItem",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": "arn:aws:dynamodb:us-east-1:121505435355:table/tf_visitorcounttable"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:us-east-1:121505435355:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "logs:CreateLogGroup",
+            "Resource": "*"
+        }
+    ]
+})
+}
+
+# create attachment of the role to the policy
+resource "aws_iam_role_policy_attachment" "tf_LambdaDynamoDBPolicy_attachment" {
+    role = aws_iam_role.tf_LambdaDynamoDBRole.name
+    policy_arn = aws_iam_policy.tf_LambdaDynamoDBPolicy.arn
 }
