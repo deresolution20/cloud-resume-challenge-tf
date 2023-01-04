a64ad704b68a6477d26a60d434b4fb0af377e16b
deresolution20 <85902399+deresolution20@users.noreply.github.com>
2023-01-04 12:40:59 -0700

    testing pipeline

--- main.tf
+++ main.tf
@@ -10,26 +10,26 @@ terraform {
 
 # configure the provider
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 ## DYNAMODB
 # create the dynamodb table
 resource "aws_dynamodb_table" "tf_visitorcounttable" {
-    name = "tf_visitorcounttable"
-    hash_key = "record_id"
-    billing_mode = "PAY_PER_REQUEST"
-    attribute {
-      name = "record_id"
-      type = "S"
-    }
+  name         = "tf_visitorcounttable"
+  hash_key     = "record_id"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "record_id"
+    type = "S"
+  }
 }
 
 # create the one and only item this table needs
 resource "aws_dynamodb_table_item" "tf_visitorcounttable_items" {
-    table_name = aws_dynamodb_table.tf_visitorcounttable.name
-    hash_key = aws_dynamodb_table.tf_visitorcounttable.hash_key
-    item = <<EOF
+  table_name = aws_dynamodb_table.tf_visitorcounttable.name
+  hash_key   = aws_dynamodb_table.tf_visitorcounttable.hash_key
+  item       = <<EOF
         {
             "record_id":{"S": "lol"},
             "record_count":{"N": "551"}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
 region = "us-east-1"
-label  = "hashicorp"
+label  = "hashicorps-test"
