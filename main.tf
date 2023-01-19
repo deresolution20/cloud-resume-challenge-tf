c506b07132e1a523fe30853cbdae4eadab4403c1
deresolution20 <85902399+deresolution20@users.noreply.github.com>
2023-01-18 18:20:11 -0700

    added s3 and cloudfront cdn

--- main.tf
+++ main.tf
@@ -16,13 +16,34 @@ terraform {
 
 # configure the provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
+}
+
+#creating the S3 bucket and configuring it to serve as a website with index.html as the index document.  CORS rule is applied
+
+resource "aws_s3_bucket" "resume_website" {
+  bucket = "cloud-resume-challenge-bneal"
+
+  website {
+    index_document = "index.html"
+  }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers = ["ETag",
+    "x-amz-meta-custom-header"]
+    max_age_seconds = 0
+  }
+}
+output "s3_website_endpoint" {
+  value = aws_s3_bucket.resume_website.website_endpoint
 }
 
 ## DYNAMODB
 # create the dynamodb table
-resource "aws_dynamodb_table" "tf_visitorcounttable" {
-  name         = "tf_visitorcounttable"
+resource "aws_dynamodb_table" "cloud-resume-stats" {
+  name         = "cloud-resume-stats"
   hash_key     = "record_id"
   billing_mode = "PAY_PER_REQUEST"
   attribute {
@@ -32,9 +53,9 @@ resource "aws_dynamodb_table" "tf_visitorcounttable" {
 }
 
 # create the one and only item this table needs
-resource "aws_dynamodb_table_item" "tf_visitorcounttable_items" {
-  table_name = aws_dynamodb_table.tf_visitorcounttable.name
-  hash_key   = aws_dynamodb_table.tf_visitorcounttable.hash_key
+resource "aws_dynamodb_table_item" "cloud-resume-stats" {
+  table_name = aws_dynamodb_table.cloud-resume-stats.name
+  hash_key   = aws_dynamodb_table.cloud-resume-stats.hash_key
   item       = <<EOF
         {
             "record_id":{"S": "lol"},
@@ -81,7 +102,7 @@ resource "aws_iam_policy" "tf_LambdaDynamoDBPolicy" {
                 "dynamodb:PutItem",
                 "dynamodb:UpdateItem"
             ],
-            "Resource": "arn:aws:dynamodb:us-east-1:121505435355:table/tf_visitorcounttable"
+            "Resource": "arn:aws:dynamodb:us-east-1:121505435355:table/cloud-resume-stats"
         },
         {
             "Effect": "Allow",
@@ -107,9 +128,9 @@ resource "aws_iam_role_policy_attachment" "tf_LambdaDynamoDBPolicy_attachment" {
 }
 
 # create lambda function
-resource "aws_lambda_function" "tf_lambdafunctionnamegoeshere" {
+resource "aws_lambda_function" "tf_incrementViewcount" {
     filename = "lambda_function.zip"
-    function_name = "tf_LambdaFunctionNameGoesHere"
+    function_name = "tf_incrementViewcount"
     role = aws_iam_role.tf_LambdaDynamoDBRole.arn
     handler = "lambda_function.lambda_handler"
     runtime = "python3.9"
@@ -120,7 +141,7 @@ resource "aws_lambda_function" "tf_lambdafunctionnamegoeshere" {
 resource "aws_lambda_permission" "tf_apigw_lambda" {
     statement_id = "AllowExecutionFromAPIGateway"
     action = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.tf_lambdafunctionnamegoeshere.function_name
+    function_name = aws_lambda_function.tf_incrementViewcount.function_name
     principal = "apigateway.amazonaws.com"
     source_arn = "arn:aws:execute-api:us-east-1:121505435355:${aws_api_gateway_rest_api.tf_APIGatewayCreatedForLambdaFunction.id}/*/${aws_api_gateway_method.tf_method.http_method}${aws_api_gateway_resource.tf_get.path}"
 
@@ -161,8 +182,8 @@ resource "aws_api_gateway_integration" "tf_integration" {
     http_method = aws_api_gateway_method.tf_method.http_method
     integration_http_method = "POST"
     type = "AWS"
-    # uri = aws_lambda_function.tf_lambdafunctionnamegoeshere.invoke_arn
-    uri = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:121505435355:function:tf_LambdaFunctionNameGoesHere/invocations"
+    # uri = aws_lambda_function.tf_incrementViewcount.invoke_arn
+    uri = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:121505435355:function:incrementViewcount"
 }
 
 # create api gateway method response
@@ -266,4 +287,57 @@ resource "aws_api_gateway_stage" "tf_stage" {
     deployment_id = aws_api_gateway_deployment.tf_apigw_deployment.id
     rest_api_id = aws_api_gateway_rest_api.tf_APIGatewayCreatedForLambdaFunction.id
     stage_name = "prod"
+}
+
+resource "aws_cloudfront_distribution" "resume_website" {
+  origin {
+    domain_name = aws_s3_bucket.resume_website.website_endpoint
+    origin_id   = "OriginName"
+    custom_origin_config {
+      origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.resume_website.arn
+    ssl_support_method  = "sni-only"
+  }
+  enabled          = true
+  is_ipv6_enabled  = true
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  aliases = ["deresolution22", "www.deresolution22.com","resume.deresolution22.com"]
+  default_cache_behavior {
+    target_origin_id       = "OriginName"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    forwarded_values {
+      query_string = false
+      headers      = ["*"]
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+}
+resource "aws_cloudfront_origin_access_identity" "resume_website" {
+  comment = "OAI for resume website"
+}
+resource "aws_acm_certificate" "resume_website" {
+  domain_name               = "deresolution22"
+  subject_alternative_names = ["*.deresolution22"]
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
